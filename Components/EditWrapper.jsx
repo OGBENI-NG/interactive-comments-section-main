d5e7909bee801f9e64188d9e55963828a8f95dce
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2023-10-24 11:54:54 +0100

    bug-fixed

--- Components/EditWrapper.jsx
+++ Components/EditWrapper.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Sections from './Sections'
 
 export default function EditWrapper({ 
       id, plusIcon, minusIcon, 
@@ -11,7 +12,7 @@ export default function EditWrapper({
       return (
             <div className="reply-wrapper">
                   <div className="scores-wrapper">
-                       <button className='plus-icon'>
+                       <button className='plus-btn'>
                               <img
                                     src={plusIcon}
                                     alt="plus-icon"
@@ -19,7 +20,7 @@ export default function EditWrapper({
                               />
                        </button>
                         <span className="scores">{score}</span>
-                        <button className='minus-icon'>
+                        <button className='minus-btn'>
                               <img
                                     className='minus'
                                     src={minusIcon}
@@ -28,23 +29,25 @@ export default function EditWrapper({
                               />
                         </button>
                   </div>
-                  {username === isCurrentUser ? (
-                        <div className="edit-wrapper">
-                              <button className="del-btn" onClick={() => handleToggleDelete(id)}>
-                                    <img src={deleteIcon} alt="delete-icon" />
-                                    delete
+                  <Sections className="delete-and-edit-wrapper">
+                        {username === isCurrentUser ? (
+                              <div className="edit-wrapper">
+                                    <button className="del-btn" onClick={() => handleToggleDelete(id)}>
+                                          <img src={deleteIcon} alt="delete-icon" />
+                                          delete
+                                    </button>
+                                    <button className="edit-btn" onClick={() => handleEditToggle(id)}>
+                                          <img src={editIcon} alt="edit-icon"  />
+                                          edit
+                                    </button>
+                              </div>
+                              ) : (
+                              <button className="reply" onClick={() => handleToggle(commentIndex)}>
+                                    <img src={replyIcon} alt="reply-icon" />
+                                    reply
                               </button>
-                              <button className="edit-btn" onClick={() => handleEditToggle(id)}>
-                                    <img src={editIcon} alt="edit-icon"  />
-                                    edit
-                              </button>
-                        </div>
-                        ) : (
-                        <button className="reply" onClick={() => handleToggle(commentIndex)}>
-                              <img src={replyIcon} alt="reply-icon" />
-                              reply
-                        </button>
-                  )}
+                        )}
+                  </Sections>
             </div>
       )
 }
